98d407cc71ae8a1197dfa1859c864dbe1791ffd0
AkshayRana92 <akshayrana@knoldus.in>
2017-02-20 15:33:39 +0530

    Enabled CORS

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var app = express();
+const cors = require('cors')
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 Todo = require('./models/todo');
 
+const corsOptions = {
+    origin: 'http://localhost:3000'
+};
+app.use(cors(corsOptions))
+
 app.use(bodyParser.json());
 
-//connect to mongoose
+//app.use(function (req, res, next) {
+//
+//    // Website you wish to allow to connect
+//    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+//
+//    // Request methods you wish to allow
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//    // Request headers you wish to allow
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//    // Set to true if you need the website to include cookies in the requests sent
+//    // to the API (e.g. in case you use sessions)
+//    res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//    // Pass to next layer of middleware
+//    next();
+//});
+////connect to mongoose
 
 mongoose.connect('mongodb://localhost/todo');
 var db = mongoose.connection;
@@ -17,7 +41,7 @@ app.get('/get/all', function(req, res){
             throw err
         }
         res.json(todoList)
-    })
+    });
 });
 
 app.get('/remove/:id', function(req, res){
